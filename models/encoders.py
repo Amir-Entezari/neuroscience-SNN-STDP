2b4573e508af3d34a63902f2e624d8a17c7b8fb1
amir <mr.amirentezari@gmail.com>
2024-04-26 02:27:47 +0330

    feat(model): define number encoder with normal dist

--- models/encoders.py
+++ models/encoders.py
@@ -42,3 +42,62 @@ class TimeToFirstSpikeEncoder(Behavior):
         result = torch.tensor([index for _, index in priority_list])
         return result
 
+
+class NumberEncoder(Behavior):
+    def initialize(self, ng):
+        self.num = self.parameter("num", None, required=True)
+        self.upper_bound = self.parameter("upper_bound", 10)
+        self.lower_bound = self.parameter("lower_bound", 0)
+        self.duration = self.parameter("duration", None, required=True)
+        self.epsilon = self.parameter("epsilon", 1e-2)
+        self.std = self.parameter("std", 1.0)
+
+        # domain = self.upper_bound - self.lower_bound
+        # self.x_values = [np.linspace(mean - (domain / 2) * self.std, mean + (domain / 2) * self.std, 1000) for mean in
+        #                  range(self.lower_bound, self.upper_bound + 1)]
+        self.x_values = torch.tensor([np.linspace(self.lower_bound, self.upper_bound, 1000) for mean in
+                                      range(self.lower_bound, self.upper_bound + 1)])
+        self.normal_dists = torch.tensor([norm.pdf(self.x_values[mean], loc=mean, scale=self.std) for mean in
+                                          range(self.lower_bound, self.upper_bound + 1)])
+
+        self.spikes = torch.zeros((self.duration, self.upper_bound - self.lower_bound + 1), dtype=torch.bool)
+        self.scheduler()
+
+    def forward(self, ng):
+        ng.spike = self.spikes[(ng.network.iteration - 1) % self.duration]
+
+    def scheduler(self):
+        spike_times = []
+        for neuron_id in range(self.lower_bound, self.upper_bound + 1):
+            pdf_at_x = self.get_pdf_at_x(self.num, mean=neuron_id)
+            if pdf_at_x>self.epsilon:
+                print(pdf_at_x)
+                spike_time = ((pdf_at_x - self.normal_dists[0].min()) / (
+                        self.normal_dists[0].max() - self.normal_dists[0].min())) * self.duration
+                spike_times.append((neuron_id, int(spike_time)))
+
+        for neuron_id, spike_time in spike_times:
+            # print(self.duration - spike_time - 1, neuron_id)
+            self.spikes[self.duration - spike_time - 1, neuron_id - self.lower_bound] = True
+
+    def create_normal_dist(self, mean, std=1):
+        x_values = np.linspace(mean - 5 * std, mean + 5 * std, 1000)
+        # Calculate the probability density at each x value
+        pdf_values = norm.pdf(x_values, loc=mean, scale=std)
+        return pdf_values
+
+    def get_pdf_at_x(self, num, mean):
+        # Calculate the probability density at x
+        pdf_at_x = norm.pdf(num, loc=mean, scale=self.std)
+        return pdf_at_x
+
+    def plot_x(self):
+        pdf_at_x_list = []
+        plt.figure(figsize=(12, 6))
+        for i in range(self.lower_bound, self.upper_bound + 1):
+            plt.plot(self.x_values[i], self.normal_dists[i])
+            pdf_at_x = self.get_pdf_at_x(self.num, mean=i)
+            pdf_at_x_list.append(pdf_at_x)
+            plt.scatter(self.num, pdf_at_x, color='r', label=f'({self.num}, {pdf_at_x:.2f})')
+        plt.axvline(x=self.num, color='r', linestyle='--', label=f'x={self.num}')
+        plt.show()
