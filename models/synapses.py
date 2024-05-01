92f445c335ed053f411ef0352ac826c6dd3af4d6
amir <mr.amirentezari@gmail.com>
2024-05-01 16:18:46 +0330

    feat(model): add STDP function and parameters to FullyConnectedSynapse model

--- models/synapses.py
+++ models/synapses.py
@@ -23,9 +23,17 @@ class FullyConnectedSynapse(Behavior):
     """
 
     def initialize(self, sg):
+        # Weight parameters
         self.j0 = self.parameter("j0", None, required=True)
         self.variance = self.parameter("variance", None)
         self.alpha = self.parameter("alpha", 1.0)
+
+        # Trace parameters
+        self.tau_pre = self.parameter("tau_pre", None, required=True)  # Presynaptic trace decay constant
+        self.tau_post = self.parameter("tau_post", None, required=True)  # Postsynaptic trace decay constant
+        # Parameters of A- and A+
+        self.eta = self.parameter("eta", 1.0)  # Adding eta for weight change control
+        self.w_max = self.parameter("w_max", 1.0)  # Maximum weight for hard bounds
 
         self.N = sg.src.size
         mean = self.j0 / self.N
@@ -34,6 +42,12 @@ class FullyConnectedSynapse(Behavior):
             self.variance = self.j0 / np.sqrt(self.N)
         else:
             self.variance = abs(mean) * self.variance
+
+        # initial value of x and y
+        if not hasattr(sg, 'x'):
+            sg.x = sg.src.vector(0.0)  # Presynaptic trace
+        if not hasattr(sg, 'y'):
+            sg.y = sg.dst.vector(0.0)  # Postsynaptic trace
 
         sg.W = sg.matrix(mode=f"normal({mean},{self.variance})")
         # Make the diagonal zero
@@ -45,6 +59,29 @@ class FullyConnectedSynapse(Behavior):
         pre_spike = sg.src.spike
         # sg.I = torch.sum(sg.W[pre_spike], axis=0)
         sg.I += torch.sum(sg.W[pre_spike], axis=0) - sg.I * self.alpha
+
+        # update traces:
+        sg.x += (-sg.x / self.tau_pre + sg.src.spike.byte()) * sg.network.dt
+        sg.y += (-sg.y / self.tau_post + sg.dst.spike.byte()) * sg.network.dt
+
+        # Update weights
+        sg.W += sg.y * -self.soft_bound_A_minus(sg.W) * sg.dst.spike.byte() + sg.x * self.soft_bound_A_plus(sg.W) * sg.src.spike.byte()
+
+    def soft_bound_A_plus(self, w):
+        """ Calculate A+ for soft bounds for a matrix of weights """
+        return (self.w_max - w) ** self.eta
+
+    def soft_bound_A_minus(self, w):
+        """ Calculate A- for soft bounds for a matrix of weights """
+        return w ** self.eta
+
+    def hard_bound_A_plus(self, w):
+        """ Calculate A+ for hard bounds for a matrix of weights """
+        return np.heaviside(self.w_max - w, 0) * (self.w_max - w) ** self.eta
+
+    def hard_bound_A_minus(self, w):
+        """ Calculate A- for hard bounds for a matrix of weights """
+        return np.heaviside(w, 0) * w ** self.eta
 
 
 class RandomConnectedFixedProbSynapse(Behavior):
@@ -125,4 +162,3 @@ class RandomConnectedFixedInputSynapse(Behavior):
             mask[selected_rows, col] = False  # Set selected elements to 1
 
         return mask  # Apply the mask to the original matrix
-
